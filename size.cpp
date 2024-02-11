7ef07639a13f36868e2edc42859dfef2e00bc4e3
c++ codes <rohitdudi7023@gmail.com>
2024-02-11 09:02:31 +0530

    first c++ code

---
+++ size.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a, b;
+    a = 10;
+    cout << ("int value: %d", a) << endl;
+    cout << "size of a:" << sizeof(a) << endl;
+    cout << "size of b:" << sizeof(b) << endl;
+}
