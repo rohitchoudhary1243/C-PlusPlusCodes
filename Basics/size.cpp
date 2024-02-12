f69c6cf3cbf14fcf68d8967f5e4c8867a7db485f
c++ codes <rohitdudi7023@gmail.com>
2024-02-12 08:36:34 +0530

    input output

---
+++ Basics/size.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a, b;
+    a = 10;
+    float c;
+    char d;
+    bool e;
+    cout << ("int value: %d", a) << endl;
+    cout << "size of int:" << sizeof(a) << endl;
+    cout << "size of float:" << sizeof(c) << endl;
+    cout << "size of char:" << sizeof(d) << endl;
+    cout << "size of bool:" << sizeof(e) << endl;
+}
---
+++ Basics/sum.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int value1;
+    int value2;
+    cout << "enter firsat value";
+    cin >> value1;
+    cout << "enter second value";
+    cin >> value2;
+    int sum = value1 + value2;
+    cout << ("the sum is", sum);
+}
