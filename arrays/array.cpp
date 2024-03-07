9f3c56186f81570893afee531230935b1fe544c6
Rohit Choudhary <148307867+rohitchoudhary1243@users.noreply.github.com>
2024-03-07 12:30:10 +0530

    arrays

---
+++ arrays/array.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int marks[] = {20, 30, 40, 50};
+    cout << " THESE ARE MARKS " << endl;
+    cout << marks[0] << endl;
+    cout << marks[1] << endl;
+    cout << marks[2] << endl;
+    cout << marks[3] << endl;
+
+    int mymarks[4];
+    mymarks[0] = 90;
+    mymarks[1] = 90;
+    mymarks[2] = 91;
+    mymarks[3] = 91;
+    cout << "my marks" << endl;
+    cout << mymarks[0] << endl;
+    cout << mymarks[1] << endl;
+    cout << mymarks[2] << endl;
+    cout << mymarks[3] << endl;
+
+    return 0;
+}
