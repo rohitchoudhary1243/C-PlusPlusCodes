3d31cb236ff01b4094f6399a4b9050a1a9b4ca88
Rohit Choudhary <148307867+rohitchoudhary1243@users.noreply.github.com>
2024-03-09 11:19:18 +0530

    Default Argument in functions

---
+++ functions/defaultArgument.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int moneyinterest(int money, float interest = 1.04)
+{
+    return money * interest;
+}
+int vipInterest(int money, float interest = 1.1)
+{
+    return money * interest;
+}
+int main()
+{
+    int money = 100000;
+    cout << "if you have " << money << " rs in your bank then yoy will recieve " << moneyinterest(money) << endl;
+    cout << "for vip: if you have " << money << " rs in your bank then yoy will recieve " << vipInterest(money) << endl;
+    return 0;
+}
